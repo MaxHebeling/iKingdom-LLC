82c64240495b6e002ace1f269368f342c7cf0db9
EliteControlGroupLLC <elitecontrolgroupllc@gmail.com>
2026-04-10 01:18:54 -0700

    Fix stale SectionDivider import in page.tsx after revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ import AgentTrace from "@/components/AgentTrace";
 import PulseTicker from "@/components/PulseTicker";
 import ChatWidget from "@/components/ChatWidget";
 import EngagementTracker from "@/components/EngagementTracker";
-import SectionDivider from "@/components/SectionDivider";
 
 export default function Home() {
   return (
@@ -23,19 +22,12 @@ export default function Home() {
       <PulseTicker />
       <main className="relative">
         <Hero />
-        <SectionDivider number="01" label="Method" />
         <Method />
-        <SectionDivider number="02" label="Capabilities" />
         <Capabilities />
-        <SectionDivider number="03" label="Continuity" />
         <Continuity />
-        <SectionDivider number="04" label="Process" />
         <Process />
-        <SectionDivider number="05" label="Live System" />
         <OperationsConsole />
-        <SectionDivider number="06" label="Proof" />
         <Proof />
-        <SectionDivider number="07" label="Application" />
         <Application />
       </main>
       <Footer />
